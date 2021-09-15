dbf60cd9f5839e0371ad74bcfdc5f1bf150066f2
Kelvin Choi <kelvinchoi95@gmail.com>
2021-09-14 17:01:30 -0700

    slowly add new home

--- pages/home.js
+++ pages/home.js
@@ -29,7 +29,28 @@ function Home() {
     <>
     <div>
         <h1>IN HOMEPAGE</h1>
-        
+        <div class="container-fluid py-5 " style={{ backgroundImage: "url(/img/homepageImage.png)"}}>
+
+                <div class="row py-5">
+                    
+
+                    <div class="col-lg-7 m-auto pt-5 text-center">
+                        
+                        
+                        <div>
+                        <h1 className="pt-5">Discover New Career Paths and Gain Insight</h1>
+
+                        <Link href={`/${user.username}`} className="btn btn-primary">
+                        
+                            <Button style={{borderRadius: '50px'}}>Find Your Sunbae</Button>
+                        </Link>
+                        </div> 
+                            
+                    </div>
+
+                </div>
+
+            </div>
     </div>
     </>
   );
